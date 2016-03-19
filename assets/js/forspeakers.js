412014e731b930bdb460487f1cf4c057a3de6053
ostrekalovsky <o.strekalovsky@yandex.ru>
2016-03-19 13:35:58 +0300

    Simplify header & navigation

---
+++ assets/js/forspeakers.js
@@ -0,0 +1,16 @@
+// Sticky header
+$(window).scroll(function () {
+    if ($(this).scrollTop() >= 50) {
+        $('.head').addClass("sticky");
+        $('header').addClass("p-top");
+    }
+    else {
+        $('.head').removeClass("sticky");
+        $('header').removeClass("p-top");
+    }
+});
+// Scroll top
+$('.logo').click(function (e) {
+    $.scrollTo('#index', 600);
+    e.preventDefault();
+});
